2aa6369ffd4240c7f310db75c5f753311730d151
jorritboer <jorritdeboer@MacBook-Pro-van-Jorrit.local>
2020-05-26 23:46:50 +0200

    Add basic bot

---
+++ bot.py
@@ -0,0 +1,27 @@
+import discord
+from secret import TOKEN
+from discord.ext import commands
+
+description = '''ninjaBot in Python'''
+bot = commands.Bot(command_prefix='?', description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.command()
+async def hello(ctx):
+    """Says world"""
+    await ctx.send("world")
+
+
+@bot.command()
+async def add(ctx, left : int, right : int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run(TOKEN)
